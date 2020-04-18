1004909455c88e3d6e0230c4cf2d23710747e0d3
madina <madina.saidova@outlook.com>
2020-04-18 23:13:28 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sortvisualization/InputFilterMinMax.kt
@@ -0,0 +1,32 @@
+package com.example.sortvisualization
+
+import android.text.InputFilter
+import android.text.Spanned
+
+
+class InputFilterMinMax(
+    private var min: Int,
+    private var max: Int
+) : InputFilter {
+
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned,
+        dstart: Int,
+        dend: Int
+    ): CharSequence? {
+        try {
+            val input = (dest.toString() + source.toString()).toInt()
+            if (isInRange(min, max, input)) return null
+        } catch (nfe: NumberFormatException) {
+        }
+
+        return ""
+    }
+
+    private fun isInRange(a: Int, b: Int, c: Int): Boolean {
+        return if (b > a) c in a..b else c in b..a
+    }
+}
